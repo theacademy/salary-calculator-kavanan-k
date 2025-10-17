205ce6e4b74acea3863d7f5f9ddf94a2357ce248
kavanan-k <kavanankaruna@gmail.com>
2025-10-17 15:33:04 +0100

    Commit 1
    Added solution.

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -59,14 +59,21 @@ public class EmployeeSalaryCalculator
 				if(HW <= standardWorkingHours)
 				{
 					//YOUR CODE STARTS HERE
-					 
+					 basicPay = HW * HR;
+                     overTimePay = 0;
+                     grossSalary = basicPay;
+
 
 					//YOUR CODE ENDS HERE
 				}
 				else
 				{
 					//YOUR CODE STARTS HERE
- 
+                    basicPay = HR * standardWorkingHours;
+                    overTime = HW - standardWorkingHours;
+                    overTimeRate = 2 * HR;
+                    overTimePay = overTime * overTimeRate;
+                    grossSalary = basicPay + overTimePay;
 
 					//YOUR CODE ENDS HERE
 				}
